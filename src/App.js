f7fd1a11b59cc90da20a5f034a994788193c2d78
Hassan Lash <hlasheen21@gmail.com>
2022-12-28 14:45:51 +0200

    Login Page UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import RadicalX from './imgs/RadicallX-Black-Logo 1.svg';
+import { Icon } from 'react-icons-kit';
+import {eyeOff} from 'react-icons-kit/feather/eyeOff';
+import {eye} from 'react-icons-kit/feather/eye';
+import {lock} from 'react-icons-kit/feather/lock'
 
 function App() {
+
+  const [type, setType] = useState('password');
+  const [icon, setIcon] = useState(eye);
+
+  const togglePassword = () => {
+    if(type === "password") {
+      setIcon(eyeOff);
+      setType("text");
+    } else {
+      setIcon(eye);
+      setType("password");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='left'></div>
+      <div className='right'>
+        <img
+          className='right__image' 
+          src={RadicalX} alt='' 
+         />
+        <form className='right__form'>
+          <p className='right__header'>Login</p>
+          <input
+            className='right__input' 
+            type="email"
+            placeholder='Email'
+          />
+          <div className='right__pass-container'>
+            <span id='lock'><Icon icon={lock} size={20}></Icon></span>
+            <input
+              className='right__input' 
+              type={type}
+              placeholder='Password'
+            />
+            <span id='eye' onClick={togglePassword}><Icon icon={icon} size={20} /></span>
+          </div>
+          <div className='right__remember-container'>
+            <div className='right__remember'>
+              <input 
+                type="checkbox"
+              />
+              <label>Remember me</label>
+            </div>
+
+            <p>Forgot password?</p>
+          </div>
+          <button
+            className='right__button'
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBK4tShCr6m3bjoH04i5zCe0hIKhq6DmSU",
+  authDomain: "radical-x-apprenticeship.firebaseapp.com",
+  projectId: "radical-x-apprenticeship",
+  storageBucket: "radical-x-apprenticeship.appspot.com",
+  messagingSenderId: "325968477669",
+  appId: "1:325968477669:web:d471bac0b418d2d2aa430c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
