20509d082871c7245a94a5f4e7bad31cfad87ebf
Hassan Lash <hlasheen21@gmail.com>
2023-01-05 14:56:19 +0200

    radio button

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_AUTH_DOMAIN,
-  projectId: process.env.REACT_PROJECT_ID,
-  storageBucket: process.env.REACT_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_ID 
+  apiKey: "AIzaSyBK4tShCr6m3bjoH04i5zCe0hIKhq6DmSU",
+  authDomain: "radical-x-apprenticeship.firebaseapp.com",
+  projectId: "radical-x-apprenticeship",
+  storageBucket: "radical-x-apprenticeship.appspot.com",
+  messagingSenderId: "325968477669",
+  appId: "1:325968477669:web:d471bac0b418d2d2aa430c" 
 };
 
 // Initialize Firebase
